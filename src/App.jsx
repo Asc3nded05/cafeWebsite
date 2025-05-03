78fbec03ff7e80afa3017459f2c9c6c58f0598f5
Riley Van Heukelum <rileyvanheukelum@gmail.com>
2025-05-03 11:01:10 -0400

    updated the favicon and other small tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -16,33 +16,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import Footer from './components/Footer'
-import Navigation from './components/Navigation'
-import NoPage from './pages/NoPage'
-import NavigationAdmin from "./components/NavigationAdmin";
-import { useEffect, useState } from "react";
-
-// const getRoleFromToken = () => {
-//   const token = localStorage.getItem('authToken');
-//   if (token) {
-//     try {
-//       const decodedToken = jwtDecode(token);
-//       return decodedToken?.role || 'guest'; // Default to 'guest' if no role
-//     } catch (error) {
-//       console.error('Error decoding token:', error);
-//       return 'guest'; // Return 'guest' or handle the error as needed
-//     }
-//   }
-//   return 'guest'; // Default role if no token
-// };
 
 function App() {
-  // const [Nav, setNav] = useState(getViewOnRole()); // Initialize Nav
-  //   useEffect(() => {
-  //       // Re-calculate Nav whenever the role state changes
-  //       setNav(getViewOnRole());
-  //   }, [])
-  // const [count, setCount] = useState(0)
-  
   return (
     
     <div id="root" className="d-flex flex-column min-vh-100">
